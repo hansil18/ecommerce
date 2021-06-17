a63ff67f79b0e976ff5b501248c2738ed5de7132
hansil18 <hansiljasoliya@gmail.com>
2021-06-17 22:25:01 +0530

    3

--- web/src/components/home/Home.jsx
+++ web/src/components/home/Home.jsx
@@ -1,23 +1,67 @@
+import { Box, makeStyles } from '@material-ui/core';
+
+//component
 import Navbar from './Navbar'
-import Banner from './Banner'
-import Slide from './Slide'
-import { Box,makeStyles} from '@material-ui/core';
-const useStyle = makeStyles({
-    component:{
+import Banner from './Banner';
+import Slide from './Slide';
+import Midsection from './Midsection';
+
+// import { products } from '../../constants/data';
+
+
+const useStyle = makeStyles ({
+    component: {
         padding: 10,
         background: '#f2f2f2'
+    },
+    rightWrapper: {
+        background: '#ffffff',
+        padding: 5,
+        margin: '12px 0 0 10px', 
+        width: '17%'
     }
 })
+
 const Home = () => {
     const classes = useStyle();
+    const adURL = 'https://rukminim1.flixcart.com/flap/464/708/image/633789f7def60050.jpg?q=70';
+
     return (
         <div>
             <Navbar />
             <Box className = {classes.component}>
                 <Banner />
+                <Box style={{display: 'flex'}}>
+                    <Box style={{width: '83%'}}>
+                        <Slide 
+                            timer={true}
+                            title="Deal of the Day"
+                        />
+                    </Box>
+                    <Box className = {classes.rightWrapper}>
+                        <img src={adURL} style={{width:230}} />
+                    </Box>
+                </Box>
+                <Midsection />
+                <Slide 
+                    timer = {false} 
+                    title="Discounts for You"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Suggested Items"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Top Selection"
+                />
+                <Slide 
+                    timer = {false} 
+                    title="Bestsellers"
+                />
             </Box>
-            <Slide />
         </div>
     )
 }
+
 export default Home;
--- web/src/components/home/Slide.jsx
+++ web/src/components/home/Slide.jsx
@@ -1,12 +1,15 @@
+  
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
-import { makeStyles,Box,Typography} from '@material-ui/core';
-import Products, { products } from '../../constants/data';
+import { makeStyles, Box, Typography, Button, Divider } from '@material-ui/core';
+import Countdown from 'react-countdown'
+import { Link } from 'react-router-dom';
+import { products } from '../../constants/data';
 
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 5
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -19,42 +22,91 @@ const responsive = {
 };
 
 const useStyle = makeStyles({
+    component: {
+        marginTop: 12,
+        background: '#ffffff',
+    },
+    deal: {
+        padding: '15px 20px',
+        display: 'flex'
+    },
+    dealText: {
+        fontSize: 22,
+        fontWeight: 600,
+        lineHeight: '32px',
+        marginRight: 25
+    },
+    timer: {
+        color: '#7f7f7f',
+        marginLeft: 10,
+        display: 'flex',
+        alignItems: 'center'
+    },
     image: {
         height: 150
     },
-    component: {
-        marginTop: 12,
-        background: 'white'
+    button: {
+        marginLeft: 'auto',
+        background: '#2874f0',
+        borderRadius: 2,
+        fontSize: 13
     },
-    deal: {
-        padding: '15px 20px'
+    text: {
+        fontSize: 14,
+        marginTop: 5
+    },
+    wrapper: {
+        padding: '35px 15px'
     }
 })
-const Slide = () => {
+
+const Slide = ({ timer, title }) => {
     const classes = useStyle();
     const timerURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg';
+
+    const renderer = ({hours, minutes, seconds}) => {
+        return <span className={classes.timer}>{hours} : {minutes} : {seconds} Left</span>;
+    }
+
     return (
-        <Box className = {classes.component}>
-            <Box className = {classes.deal}>
-                <Typography>Deal of the day</Typography>
+        <Box className={classes.component}>
+            <Box className={classes.deal}>
+                <Typography className={classes.dealText} >{title}</Typography>
+                {
+                    timer && 
+                    <>
+                        <img src = {timerURL} style={{width:24}}  />
+                        <Countdown date={Date.now() + 5.04e+7} renderer={renderer} />
+                        <Button variant="contained" color="primary" className={classes.button}>View All</Button>
+                    </>
+                }
             </Box>
-            <Carousel
+            <Divider/>
+            <Carousel 
                 responsive={responsive}
-                infinite = {true}
-                draggable = {false}
-                swipeable = {false}
-                centerMode = {true}
-                autoPlay =  {true}
-                autoPlaySpeed = {10000}
-                keyBoardControl = {true}
-                removeArrowOnDeviceType = {["tablet","mobile"]}
-                containerClass="carousel-container"
+                infinite={true}    
+                draggable={false}
+                swipeable={false}
+                centerMode={true}
+                autoPlay={true}
+                autoPlaySpeed={10000}
+                keyBoardControl={true}
+                showDots={false}
+                removeArrowOnDeviceType={["tablet", "mobile"]}
                 dotListClass="custom-dot-list-style"
                 itemClass="carousel-item-padding-40-px"
+                containerClass="carousel-container"
             >
                 {
-                    products.map(products => (
-                        <img src = {products.url} className = {classes.image}/>
+                    products.map(product => (
+                        <Link to={`product/${product.id}`}>
+                            <Box textAlign="center" className={classes.wrapper}>
+                                <img src = {product.url} className = {classes.image} />
+                                <Typography className={classes.text} style={{fontWeight:600, color:'#212121'}}>{product.title.shortTitle}</Typography>
+                                <Typography className={classes.text} style={{color:'green'}}>{product.discount}</Typography>
+                                <Typography className={classes.text} style={{color:'#212121', opacity: '.6'}}>{product.tagline}</Typography>
+                            </Box>
+                        </Link>
                     ))
                 }
             </Carousel>
